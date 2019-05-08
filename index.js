ab58af7e66dfca9908b4506dfd3dff829d117ab4
adam <bcuz@users.noreply.github.com>
2019-05-08 10:58:13 -0400

    Hello World

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+// const { hubs } = db;
+
+server.use(express.json());
+
+server.get('/', (req,res) => {
+
+    res.send("<h2>Hello World</h2>")
+})
+
+server.listen(5001, () => {
+  console.log('Server is listening');
+})
