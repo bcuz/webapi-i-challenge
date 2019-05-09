fddf755579aebee419d5e5d9b97728bb304ca929
adam <bcuz@users.noreply.github.com>
2019-05-08 21:38:04 -0400

    get user. error handling needs more work

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ server.get('/api/users', (req,res) => {
   })
 })
 
+server.get('/api/users/:id', (req, res, next) => {
+  const { id } = req.params;
+  
+  db.findById(id)
+  .then(user => {
+
+    if (user) {
+      res.json(user)
+    }
+
+    // not sure if the best way
+    next(new Error("The user with the specified ID does not exist."))
+    
+  })
+  // .catch((err) => {    
+  //     res.status(404).json({ message: err })
+  // })
+
+})
+
 server.listen(5001, () => {
   console.log('Server is listening');
 })
